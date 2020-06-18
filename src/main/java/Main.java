de63e7cd0129d354da68fb09f3bb87e3a98f8561
Ivan Grebenschikov <albonigrum@gmail.com>
2020-06-18 23:01:42 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, registered);
+    System.out.println("Бонус: " + service.calculate(amount, registered));
   }
 }
 
